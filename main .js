b21a863504ccbd29aeb92701876646df6f96c7f5
dknyakundi <dknyakundi1@gmail.com>
2024-01-15 13:23:59 +0300

    More methods to deal with arrays

--- main .js
+++ main .js
@@ -32,3 +32,47 @@ const mienne = `My name is ${myName} and I am turning ${age} this year`;
 
 console.log(mienne);
  
+
+//methods - function associated with an object - followed by brackets
+const s = 'Hello World!'
+
+console.log(s.length); //length
+console.log(s.toUpperCase());
+console.log(s.substring(1,8));
+
+//for instance I want to extract tags then save them in a database
+const y = 'react, vue, angular, node, native'
+
+console.log(y.split(', ')) //prints the resulting split array
+
+//arrays
+//Creating an array 
+// method 1
+const numbers = new Array(1,2,3,4, 'ten', 'JavaScript');
+console.log(numbers);
+
+//method 2
+const cars = ['Tesla', 'BMW', 'Mercedes', 'Porsche'];
+console.log(cars);
+
+//Adding an item to the array
+//method 1
+cars[4] = 'Toyota';
+//method 2 - adds to the end of existing list
+cars.push('Mazda');
+//method 3 - adds to the beginning of existing list
+cars.unshift('Land Rover');
+
+//Remove the last item in the array
+cars.pop();
+
+//Checking if something is an array
+console.log(Array.isArray(cars));
+
+//Checking the index of an item in array
+console.log(cars.indexOf('BMW'));
+
+
+
+
+
