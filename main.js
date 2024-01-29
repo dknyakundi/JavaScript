e63f9e6d968c240e88f8d96737dea2546dc45e2f
dknyakundi <dknyakundi1@gmail.com>
2024-01-29 11:12:56 +0300

    more functionaltiy to the form

--- main.js
+++ main.js
@@ -320,19 +320,59 @@ const ul = document.querySelector('.items');
 
 // ul.remove();
 // ul.lastElementChild.remove();
-ul.firstElementChild.textContent = 'WaDaniel';
-ul.children[1].innerText = 'DK';
+// ul.firstElementChild.textContent = 'WaDaniel';
+// ul.children[1].innerText = 'DK';
 
-ul.lastElementChild.innerHTML = '<h1>Hey</h1>';
+// ul.lastElementChild.innerHTML = '<h1>Hey</h1>';
 
 //changing style
-const btn =document.querySelector('.btn');
-btn.style.background = 'blue';
+// const btn =document.querySelector('.btn');
+// btn.style.background = 'blue';
 
 //events
-btn.addEventListener('mouseout', (e) => {
+// btn.addEventListener('mouseout', (e) => {
+//     e.preventDefault();
+//     document.querySelector('#my-form').style.background = '#ccc';
+//     document.querySelector('body').classList.add('bg-dark');
+//     document.querySelector('.items').lastElementChild.innerHTML = '<h2>Wozza</h2>';
+// })
+
+//playing with the form
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit' ,onSubmit);
+
+function onSubmit(e) {
     e.preventDefault();
-    document.querySelector('#my-form').style.background = '#ccc';
-    document.querySelector('body').classList.add('bg-dark');
-    document.querySelector('.items').lastElementChild.innerHTML = '<h2>Wozza</h2>';
-})
+
+    // console.log(nameInput.value); //adding the .value section logs the exact content of the input field chosen
+
+    //ensuring that all the input fields are filled before submiting
+
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        setTimeout(() => msg.remove(), 3000); //error message to go away after a defined number of millisecondsx
+    }
+    else {
+        console.log('success');
+
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`
+        ${nameInput.value} : ${emailInput.value}
+        `));
+
+        userList.appendChild(li);
+
+        //clear the input fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+
+}
+
