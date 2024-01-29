9f4c40205b0af6a1cdecbb259037f8c34813b5db
dknyakundi <dknyakundi1@gmail.com>
2024-01-29 10:28:10 +0300

    checkout more events on MDN docs

--- main.js
+++ main.js
@@ -330,7 +330,7 @@ const btn =document.querySelector('.btn');
 btn.style.background = 'blue';
 
 //events
-btn.addEventListener('click', (e) => {
+btn.addEventListener('mouseout', (e) => {
     e.preventDefault();
     document.querySelector('#my-form').style.background = '#ccc';
     document.querySelector('body').classList.add('bg-dark');
